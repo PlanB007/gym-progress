31b242194a301f0b0b7badbf9af706a3a5eb574d
Kevin Rouwkema <kevinrouwkema@gmail.com>
2018-09-16 21:30:19 +0200

    Created first setup

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+console.log('script file working');
+
+
+import GetSheetDone from "get-sheet-done";
+
+var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit#gid=0';
+
+
+GetSheetDone.raw(publicSpreadsheetUrl).then(sheet => {
+    console.log(sheet);
+});
