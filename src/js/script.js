7ada48de9f2369cfee4521da0cb5dbe7e880de6e
info@noudadrichem.com <info@noudadrichem.com>
2018-09-17 10:10:19 +0200

    your bundler is working buddy

--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,12 @@
 console.log('script file working');
 
 import GetSheetDone from 'get-sheet-done';
+import Tabletop from 'tabletop'
 
-var publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit#gid=0';
+const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit#gid=0';
 
 
-GetSheetDone.raw(publicSpreadsheetUrl).then(sheet => {
-    console.log(sheet);
-});
+// GetSheetDone.raw(publicSpreadsheetUrl)
+  // .then(sheet => {
+  //     console.log(sheet);
+  // });
