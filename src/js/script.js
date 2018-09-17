2bff3205ac27a02f50e955f90c60de47e3ba926b
Kevin Rouwkema <kevinrouwkema@gmail.com>
2018-09-17 23:35:15 +0200

    fetched data and logged it

--- src/js/script.js
+++ src/js/script.js
@@ -3,10 +3,33 @@ console.log('script file working');
 import GetSheetDone from 'get-sheet-done';
 import Tabletop from 'tabletop'
 
-const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit#gid=0';
+let gymExercises = [];
+const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit?usp=sharing';
 
+function init() {
+    Tabletop.init( { key: publicSpreadsheetUrl,
+                     callback: showInfo,
+                     simpleSheet: true } )
+  }
 
-// GetSheetDone.raw(publicSpreadsheetUrl)
-  // .then(sheet => {
-  //     console.log(sheet);
-  // });
+  function showInfo(data, tabletop) {
+    alert('Successfully processed!')
+    gymExercises.push(...data);
+
+    let gymExerciseWeek = gymExercises.map(gymExercise => gymExercise.Week);
+    console.log(gymExerciseWeek);
+
+    let gymExerciseNames = gymExercises.map(gymExercise => gymExercise.Oefening);
+    console.log(gymExerciseNames);
+
+    let gymExerciseWeight = gymExercises.map(gymExercise => gymExercise.Gewicht);
+    console.log(gymExerciseWeight);
+
+    let gymExercisesInfo = gymExercises.map(gymExercises => [gymExercises.Week, gymExercises.Oefening, gymExercises.Gewicht]);
+    //console.log(gymExercisesInfo);
+
+    let gymExerciseInfo = gymExercisesInfo.map(gymExerciseInfo => console.log(gymExerciseInfo));
+
+  }
+
+window.addEventListener('DOMContentLoaded', init)
