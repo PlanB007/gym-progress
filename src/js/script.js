416b75ddda00870227685ee433175b5316f73d8a
Kevin Rouwkema <kevinrouwkema@gmail.com>
2018-09-24 17:44:16 +0200

    fixed Promise bug & added reject handler

--- src/js/script.js
+++ src/js/script.js
@@ -5,16 +5,31 @@ import ReactDOM from 'react-dom';
 import GetSheetDone from 'get-sheet-done';
 import Tabletop from 'tabletop';
 
+
+class Board extends React.Component {
+  render() {
+    return (
+      <div className="progress-board">
+      </div>
+    );
+  }
+}
+
 const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzca-4dpiiXyhi2T6eB-2_V8s4U3ZRnnCfaU/edit?usp=sharing';
 
 function getData() {
-  console.log('getdata')
-  return new Promise((resolve) => {
-    Tabletop.init({key: publicSpreadsheetUrl, callback: showInfo, simpleSheet: true})
-    resolve('Done');
+  return new Promise((resolve, reject) => {
+    Tabletop.init({key: publicSpreadsheetUrl,
+      callback: function (data, tabletop) { resolve(showInfo(data, tabletop)); },
+      simpleSheet: true})
+      console.log('promsie running');
+      Promise.reject(new Error('fail')).then(function() {
+        // not called
+      }, function(error) {
+        console.log(error); // Stacktrace
+      });
   })
 }
-
 // old fashion way keep this here aslong the function on line 28 doesn't work
 // function showInfo (data, tabletop) {
 //   console.log('Successfully processed!');
@@ -26,42 +41,44 @@ function getData() {
 // }
 let arrayWithData = [];
 
+// showInfo() get runned twice
 function showInfo (data, tabletop) {
   console.log('showInfo active');
   arrayWithData.push(...data);
-  return new Promise(resolve => {
-    console.log(arrayWithData, 'data is here')
-    resolve(arrayWithData) // This doesn't work yet
-  })
+  console.log(arrayWithData, 'data is here')
+  return arrayWithData;
 }
 
- //showInfo().then(data => {
-  //  console.log(data, 'data from the Promise')
-  //}) // This doensn't work
+ getData()
+  .then(data => {
+   console.log(data, 'data from the Promise')
+  })
+  .then(function renderReactToDom(hint) {
+   ReactDOM.render(<Board/>, document.querySelector('#root'))
+  })
+   // This doesn't work
 
-getData()
+// getData()
 //  This will render the reactblock
-//  .then(function renderReactToDom(hint) {
-//   ReactDOM.render(<Board/>, document.querySelector('#root'))
-// })
 
-function promiseChain() {
-  return new Promise(resolve => {
-    resolve(1)
-  })
-}
 
-promiseChain()
-  .then(thisIsOne => {
-    console.log(thisIsOne)
-
-    return thisIsOne + 1
-  })
-  .then(thisIsTwo => {
-    console.log(thisIsTwo)
-
-    return thisIsTwo + 1
-  })
-  .then(thisIsThree => {
-    console.log(thisIsThree);
-  })
+// function promiseChain() {
+//   return new Promise(resolve => {
+//     resolve(1)
+//   })
+// }
+//
+// promiseChain()
+//   .then(thisIsOne => {
+//     console.log(thisIsOne)
+//
+//     return thisIsOne + 1
+//   })
+//   .then(thisIsTwo => {
+//     console.log(thisIsTwo)
+//
+//     return thisIsTwo + 1
+//   })
+//   .then(thisIsThree => {
+//     console.log(thisIsThree);
+//   })
