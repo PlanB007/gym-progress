1f82925fbb7796aed13eee0826392cfb466c8b49
Kevin Rouwkema <kevinrouwkema@gmail.com>
2018-10-03 22:37:32 +0200

    data can now be used inside react

--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,19 @@
-console.log('Script file working');
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import GetSheetDone from 'get-sheet-done';
 import Tabletop from 'tabletop';
 
-
 class Board extends React.Component {
   render() {
+    const week = this.props.gymProgresses.map((gymProgress, idx) => <span key={idx}>{gymProgress.Week}</span>);
+    const oefening = this.props.gymProgresses.map((gymProgress, idx) => <span key={idx}>{gymProgress.Oefening}</span>);
+    const gewicht = this.props.gymProgresses.map((gymProgress, idx) => <span key={idx}>{gymProgress.Gewicht}</span>);
+
     return (
       <div className="progress-board">
+        {week}
+        {oefening}
+        {gewicht}
       </div>
     );
   }
@@ -20,65 +24,32 @@ const publicSpreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1YlxxDzUQzc
 function getData() {
   return new Promise((resolve, reject) => {
     Tabletop.init({key: publicSpreadsheetUrl,
-      callback: function (data, tabletop) { resolve(showInfo(data, tabletop)); },
+      callback: function (data, tabletop) {
+        resolve(showInfo(data, tabletop));
+      },
       simpleSheet: true})
       console.log('promsie running');
       Promise.reject(new Error('fail')).then(function() {
-        // not called
+      // not called
       }, function(error) {
         console.log(error); // Stacktrace
       });
   })
 }
-// old fashion way keep this here aslong the function on line 28 doesn't work
-// function showInfo (data, tabletop) {
-//   console.log('Successfully processed!');
-//   let gymExercises = [];
-//   gymExercises.push(...data);
-//   console.log(gymExercises);
-//   console.log(data)
-//   return data
-// }
+
 let arrayWithData = [];
 
-// showInfo() get runned twice
 function showInfo (data, tabletop) {
-  console.log('showInfo active');
   arrayWithData.push(...data);
-  console.log(arrayWithData, 'data is here')
   return arrayWithData;
 }
 
  getData()
-  .then(data => {
-   console.log(data, 'data from the Promise')
-  })
   .then(function renderReactToDom(hint) {
-   ReactDOM.render(<Board/>, document.querySelector('#root'))
+    console.log(hint,'hitn');
+
+    ReactDOM.render(
+      <Board gymProgresses={hint} />,
+      document.querySelector('#root')
+    )
   })
-   // This doesn't work
-
-// getData()
-//  This will render the reactblock
-
-
-// function promiseChain() {
-//   return new Promise(resolve => {
-//     resolve(1)
-//   })
-// }
-//
-// promiseChain()
-//   .then(thisIsOne => {
-//     console.log(thisIsOne)
-//
-//     return thisIsOne + 1
-//   })
-//   .then(thisIsTwo => {
-//     console.log(thisIsTwo)
-//
-//     return thisIsTwo + 1
-//   })
-//   .then(thisIsThree => {
-//     console.log(thisIsThree);
-//   })
